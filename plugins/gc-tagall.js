cff0c8ed11fd615feda29918716056e5840cb3b3
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-04-05 17:07:56 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,11 +6,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀ACTIVENSEN ADORNOS⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*🐰🥕Activate te esta hablando bugs bunny🐰🥕*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
-    teks += `┣➥ @${mem.id.split('@')[0]}\n`;
+    teks += `🐰🥕 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* *By Anthony Ventas*_\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* *🐰🥕Bugs Bunny🐰🥕*_\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
