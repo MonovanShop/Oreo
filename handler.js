fa4768eac45c060928f9a2becbfadf6da67210c6
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-04-05 20:37:49 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -1546,7 +1546,7 @@ global.dfail = (type, m, conn) => {
     restrict: '*[ ℹ️ ] Este comando fue desactivado por el propietario del bot.*',
   }[type];
   const aa = {quoted: m, userJid: conn.user.jid};
-  const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '*[ ⚠ ] Advertencia*', body: 'CapiBot Premium', thumbnail: imagen1, sourceUrl: 'https://wa.me/525625060788'}}}}, aa);
+  const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '*[ ⚠ ] Advertencia*', body: 'Bugs Bunny', thumbnail: imagen1, sourceUrl: 'https://wa.me/527299350963'}}}}, aa);
   if (msg) return conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id});
 };
 
