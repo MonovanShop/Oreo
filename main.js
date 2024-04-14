ae9213583acf1981f1351cfb29abf2e3aa3a7452
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-04-13 18:49:52 -0600

    Update main.js

--- main.js
+++ main.js
@@ -543,7 +543,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime} Bugs Bunny`;
+  const bio = `[ ⏳ ] Uptime: ${uptime} OREO BOT`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
