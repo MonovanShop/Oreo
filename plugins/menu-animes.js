ab12fb2791cefcbd60a041c835f61af99a411e84
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-04-13 18:45:56 -0600

    Update menu-animes.js

--- plugins/menu-animes.js
+++ plugins/menu-animes.js
@@ -18,7 +18,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `◉— _*BUGS BUNNY*_ —◉
+    const str = `◉— _*OREO*_ —◉
 
  🐰🥕  *Hola,* ${taguser}
 
