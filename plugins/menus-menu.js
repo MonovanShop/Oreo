117597ebfb1c205596bd4d0147ffb88c0a25f81f
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-04-13 18:45:00 -0600

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -21,7 +21,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `— MONKEY —◉
+    const str = `— OREO —◉
 
  🐾🍪  Hola, ${taguser}
 
