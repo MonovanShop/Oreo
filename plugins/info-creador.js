d00655d3db3631547f8e4681e368efbed29f83d6
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-04-13 18:54:58 -0600

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,24 +1,24 @@
 const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
-  const text = `*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁 𝙴𝚂 wa.me/527299350963*`.trim();
+  const text = `*—◉ 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 𝙲𝚁𝙴𝙰𝙳𝙾𝚁 𝙴𝚂 wa.me/525610035034*`.trim();
   const buttonMessage= {
-    'document': {url: `https://github.com/MonovanShop/Bugs-Bunny.git`},
+    'document': {url: `https://github.com/MonovanShop/Oreo.git`},
     'mimetype': ` `,
-    'fileName': `「  Alquila Bugs Bunny 」`,
+    'fileName': `「  Alquila OREO BOT 」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://github.com/MonovanShop/Bugs-Bunny.git',
+        'mediaUrl': 'https://github.com/MonovanShop/Oreo.git',
         'mediaType': 2,
         'previewType': 'url',
         'title': 'Eʟ Mᴇᴊᴏʀ Bᴏᴛ ᴅᴇ Wʜᴀᴛsᴀᴘᴘ⁩',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'wa.me/527299350963'}},
+        'sourceUrl': 'wa.me/525610035034'}},
     'caption': text,
     'footer': wm,
     // 'buttons':[
